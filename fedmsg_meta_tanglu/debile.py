15d5872f00b688bfe41445f2eaa615775fc7a9f1
Jon Severinsson <jon@severinsson.net>
2014-05-11 17:03:45 +0200

    Update debile message processing

--- fedmsg_meta_tanglu/debile.py
+++ fedmsg_meta_tanglu/debile.py
@@ -21,7 +21,7 @@
 from __future__ import unicode_literals
 
 from fedmsg.meta.base import BaseProcessor
-import re
+
 
 class DebileProcessor(BaseProcessor):
     __name__ = "debile"
@@ -30,49 +30,81 @@ class DebileProcessor(BaseProcessor):
     __docs__ = "Would be nice."
     __obj__ = "Building"
 
-    event2title = {
-        'build': "Package build",
-        'result': "Results",
-        'comment': "Comment on a package (mentors)",
-        'receive': "Results received",
-        'accept': "Source accepted",
-    }
-
     def title(self, msg, **config):
         event = msg['topic'].split('.')[-1]
-        return self.event2title.get(event, event)
+        mod = msg['modname'].split('.')[-1]
+
+        if event == "start" and mod == "job":
+            return "Job assigned"
+
+        if event == "complete" and mod == "job":
+            return "Job completed"
+
+        if event == "abort" and mod == "job":
+            return "Job aborted"
+
+        if event == "receive" and mod == "result":
+            return "Job results received"
+
+        elif event == "accept" and mod == "source":
+            return "Source accepted"
+
+        elif event == "accept" and mod == "binary":
+            return "Binary accepted"
+
+        return event
 
     def subtitle(self, msg, **config):
         event = msg['topic'].split('.')[-1]
-        content = msg['msg']
-        if event == "receive":
-            state_str = "was successful"
-            if content['failed']:
-                state_str = "has failed"
-            text = "Job '%s' for package '%s' in '%s' %s." % (content['job'], content['source'], content['suite'], state_str)
-        elif event == "accept":
-            text = "Package '%s' for '%s' was accepted by the build system." % (content['source'], content['suite'])
-        else:
-            text = content
+        mod = msg['modname'].split('.')[-1]
 
-        return text
+        if event == "start" and mod == "job":
+            job = msg['msg']
+            return "Job '%s [%s]' for package '%s' in suite '%s' assigned to '%s'." % (job['name'], job['arch'], job['source'], job['suite'], job['builder']['name'])
+
+        if event == "complete" and mod == "job":
+            job = msg['msg']
+            return "Job '%s [%s]' for package '%s' in suite '%s' completed by '%s'." % (job['name'], job['arch'], job['source'], job['suite'], job['builder']['name'])
+
+        if event == "abort" and mod == "job":
+            job = msg['msg']
+            return "Job '%s [%s]' for package '%s' in suite '%s' aborted by '%s'." % (job['name'], job['arch'], job['source'], job['suite'], job['builder']['name'])
+
+        if event == "receive" and mod == "result":
+            result = msg['msg']
+            job = result['job_obj']
+            state_str = "has failed" if result['failed'] else "was successful"
+            return "Job '%s [%s]' for package '%s' in suite '%s' %s." % (job['name'], job['arch'], job['source'], job['suite'], state_str)
+
+        if event == "accept" and mod == "source":
+            source = msg['msg']
+            return "Source package '%s (%s)' in suite '%s' was accepted by the build system." % (source['name'], source['version'], source['suite'])
+
+        if event == "accept" and mod == "binary":
+            binary = msg['msg']
+            return "Binary package '%s (%s)' for arch '%s' in suite '%s' was accepted by the build system." % (binary['name'], binary['version'], binary['arch'], binary['suite'])
+
+        return msg['msg']
 
     def link(self, msg, **config):
-        content = msg['msg']
-        source = content['source']
-        event = msg['topic'].split('.')[-1]
-        if event == "receive":
-            sourcepkg = ""
-            version = ""
-            if "(" in source:
-                parts = source.split("(")
-                sourcepkg = parts[0].strip()
-                version = parts[1]
-                if ")" in version:
-                    version = version.replace(")", "").strip()
-            else:
-                return ""
-            url = "http://buildd.tanglu.org/job/%s/%s/%s/%s/" % (content['group'], sourcepkg, version, content['job_id'])
-            return url
+        mod = msg['modname'].split('.')[-1]
+
+        if mod == "job":
+            job = msg['msg']
+            source = msg['msg']['source_obj']
+
+        if mod == "result" or mod == "binary":
+            job = msg['msg']['job_obj']
+            source = msg['msg']['source_obj']
+
+        if mod == "source":
+            job = None
+            source = msg['msg']
+
+        if job:
+            return "http://buildd.tanglu.org/job/%s/%s/%s/%s/" % (source['group'], source['name'], source['version'], job['id'])
+
+        if source:
+            return "http://buildd.tanglu.org/source/%s/%s/%s/" % (source['group'], source['name'], source['version'])
 
         return None
